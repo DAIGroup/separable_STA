ccce1184598bb70938fb5c5f17cffa534a3d3acc
Pau Climent <pclimentperez@gmail.com>
2020-09-07 16:33:05 +0200

    LOCAL: Change of sta_config to work on local machine.

--- sta_config.py
+++ sta_config.py
@@ -5,5 +5,6 @@ import os
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 import tensorflow as tf
 tf.device('/gpu:0')
-code_path = '/home/paal/deploy'
+# code_path = '/home/paal/deploy'
+code_path = '/home/pau/harnets'
 gpus = 1
